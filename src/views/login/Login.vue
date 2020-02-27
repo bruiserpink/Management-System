c25190eca46152a07b430a4324d672180d00b83e
BruiserPink <1609526684@qq.com>
2020-02-27 19:00:13 +0800

    目录搭建

---
+++ src/views/login/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login"
+  }
+</script>
+
+<style scoped lang="less">
+  .login_container {
+    background: #2b4b6b;
+    height: 100vh;
+    position: relative;
+    .login_box{
+      width: 450px;
+      height: 300px;
+      background: #fff;
+      border-radius: 3px;
+      left: 50%;
+      top: 50%;
+      position: absolute;
+      transform: translate(-50%,-50%);
+    }
+  }
+</style>
